746aacd385751d532882f7caadd5a6da09765c8a
yazan-kad <yazankad64@gmail.com>
2023-02-27 21:11:33 +0200

    create profile and make it responsive

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -10,9 +10,11 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/darkMoodContext";
 import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
 const NavBar = () => {
   const { toggle, darkMode } = useContext(DarkModeContext);
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
       <div className="left">
@@ -36,8 +38,8 @@ const NavBar = () => {
         <EmailOutlinedIcon />
         <NotificationsOutlinedIcon />
         <div className="user">
-          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" />
-          <span>John Doe</span>
+          <img src={currentUser.profilePic} />
+          <span>{currentUser.name}</span>
         </div>
       </div>
     </div>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 import "./login.scss";
 
 const Login = () => {
+  const { login } = useContext(AuthContext);
+  const handelLogin = () => {
+    login();
+  };
   return (
     <div className="login">
       <div className="card">
@@ -23,7 +29,7 @@ const Login = () => {
           <form>
             <input type="text" placeholder="Username" />
             <input type="password" placeholder="Password" />
-            <button>Login</button>
+            <button onClick={handelLogin}>Login</button>
           </form>
         </div>
       </div>
